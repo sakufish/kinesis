2c35ebe289e2a16e03fc3363177931a29eae3c17
chrisma71 <chris.pengfei.ma@gmail.com>
2024-09-28 09:02:05 -0400

    react 3 fiber test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import login from './components/login/login'
 import Home from './components/Home';
 import Pose from './components/Pose';
 
