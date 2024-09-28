e5730a3d068b59c63cdfb8143be7e86a7d39755f
caden c <cadenc.woss@gmail.com>
2024-09-28 15:51:27 -0400

    update usermodel

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  userId: { type: String, unique: true, required: true }, // Unique ID
+  userId: { type: String, unique: true, required: true }, 
+  injuries: { type: [String], default: [] },
+  difficulty: { type: String, default:"Easy"}, 
+  details: { type: String, default:"" }, 
 });
 
 module.exports = mongoose.model('User', userSchema);
