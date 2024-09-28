fc90c00ca2d1ff1b8330d0fae748d8948f8bde3e
Matthew <matthewyanglele@gmail.com>
2024-09-28 09:04:06 -0400

    remove login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import login from './components/login/login'
 import Home from './components/Home';
 import Pose from './components/Pose';
 
