2517b65be97ca069b577da7d5d7a8593ad2c985b
Matthew <matthewyanglele@gmail.com>
2024-09-28 08:25:43 -0400

    add basic components for pose detection

--- frontend/src/components/Pose.tsx
+++ frontend/src/components/Pose.tsx
@@ -1,11 +1,21 @@
 import * as poseDetection from '@tensorflow-models/pose-detection';
+import Webcam from 'react-webcam';
+
+import { useEffect, useState } from 'react';
 
 const Pose = () => {
-    const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet);
+    const [detector, setDetector] = useState<poseDetection.PoseDetector | null>(null);
+    
+    useEffect(() => {
+        poseDetection.createDetector(poseDetection.SupportedModels.MoveNet).then((poseDetector) => {
+            setDetector(poseDetector);
+        });
+    }, []);
 
     return (
         <div>
             <h1>Pose Detection</h1>
+            <Webcam />
         </div>
     )
 }
