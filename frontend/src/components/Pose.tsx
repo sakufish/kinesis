7d11ff7ff2bc20dac3be62021105bc8afeadb2e3
Matthew <matthewyanglele@gmail.com>
2024-09-28 08:54:28 -0400

    add pose detection base

--- frontend/src/components/Pose.tsx
+++ frontend/src/components/Pose.tsx
@@ -1,21 +1,56 @@
 import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as tf from "@tensorflow/tfjs";
 import Webcam from 'react-webcam';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+const detectorConfig = {
+    runtime: 'tfjs',
+    modelType: 'SinglePose.Lightning',
+    modelUrl: 'https://storage.googleapis.com/tfhub-tfjs-modules/google/tfjs-model/movenet/singlepose/lightning/4/model.json'
+};
 
 const Pose = () => {
     const [detector, setDetector] = useState<poseDetection.PoseDetector | null>(null);
-    
+
+    const webcamRef = useRef<Webcam>(null);
+
     useEffect(() => {
-        poseDetection.createDetector(poseDetection.SupportedModels.MoveNet).then((poseDetector) => {
-            setDetector(poseDetector);
-        });
+        tf.setBackend("webgl").then(() => {
+            const MoveNet = poseDetection.SupportedModels.MoveNet;
+            poseDetection.createDetector(MoveNet, detectorConfig).then((detector) => {
+                setDetector(detector);
+            });
+        })
+        .catch(() => window.alert("Failed to load webgl"));
     }, []);
+
+    const getPoses = async () => {
+        if (detector && webcamRef.current && webcamRef.current.video) {
+            const webcam = webcamRef.current.video;
+            const poses = await detector.estimatePoses(webcam, {
+                maxPoses: 1,
+                flipHorizontal: false,
+                scoreThreshold: 0.6
+            });
+            console.log(poses);
+        }
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            getPoses();
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, [detector]);
 
     return (
         <div>
             <h1>Pose Detection</h1>
-            <Webcam />
+            <Webcam ref={webcamRef} videoConstraints={{
+                frameRate: { max: 30 },
+            }}/>
         </div>
     )
 }
