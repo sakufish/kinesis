8c56fd11c67d8cb7ed7a511fd020e8947eb9aa35
Matthew <matthewyanglele@gmail.com>
2024-09-29 01:38:14 -0400

    fix timer

--- frontend/src/pages/Pose.tsx
+++ frontend/src/pages/Pose.tsx
@@ -227,6 +227,7 @@ const Pose = () => {
     useEffect(() => {
         if (count >= reps) {
             setIsCounting(false);
+            setCount(0);
             moveToNextStep();
         }
     }, [count, reps]);
