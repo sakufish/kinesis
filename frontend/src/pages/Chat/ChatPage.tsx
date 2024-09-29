52168d2796f7885cdfefe65cdcd30d7ef760d348
chrisma71 <chris.pengfei.ma@gmail.com>
2024-09-29 09:48:33 -0400

    contact list

--- frontend/src/pages/Chat/ChatPage.tsx
+++ frontend/src/pages/Chat/ChatPage.tsx
@@ -5,6 +5,7 @@ import Cookies from 'js-cookie';
 import Logo from './assets/logo.png';
 import SendIcon from './assets/send.png'; // Assuming you have the send.png icon here
 import UserIcon from './assets/usersvg.svg'; // Assuming you have the user icon here
+import PlusIcon from './assets/plus.svg'; // Assuming you have the plus icon here
 import home from '../Achievements/assets/home.png';
 
 interface Message {
@@ -13,11 +14,18 @@ interface Message {
     sender: string;
 }
 
+interface Contact {
+    uuid: string;
+    name: string;
+}
+
 const ChatPage: React.FC = () => {
     const [userId] = useState<string>(Cookies.get('userId') || '');
-    const [recipientId, setRecipientId] = useState<string>('');
+    const [recipientId, setRecipientId] = useState<string>(''); 
     const [message, setMessage] = useState<string>('');
     const [messages, setMessages] = useState<Message[]>([]);
+    const [contacts, setContacts] = useState<Contact[]>([]); // Array of contacts
+    const [newContactUUID, setNewContactUUID] = useState<string>(''); // New contact UUID input
     const [senderName, setSenderName] = useState<string>('');
     const [isShrinking, setIsShrinking] = useState<boolean>(false);
 
@@ -73,81 +81,141 @@ const ChatPage: React.FC = () => {
         }
     }, [recipientId]);
 
+    const selectContact = (contactUUID: string) => {
+        setRecipientId(contactUUID); // Set recipientId to UUID for messaging
+    };
+
+    const addContact = async () => {
+        if (!newContactUUID.trim()) return;
+
+        try {
+            // Fetch the contact's name from the backend using their UUID
+            const response = await axios.get(`http://localhost:3000/api/user/${newContactUUID}`);
+            const contactName = response.data.name;
+
+            // Add the new contact to the list with their UUID and name
+            const newContact: Contact = { uuid: newContactUUID, name: contactName };
+            setContacts([...contacts, newContact]);
+
+            // Clear the input field after adding
+            setNewContactUUID('');
+        } catch (error) {
+            console.error('Error fetching contact:', error);
+        }
+    };
+
     return (
-        <div className="flex flex-col items-center justify-center p-4 h-screen bg-[#000] font-roboto">
-            <div className='absolute z-10 w-8 h-auto top-6 left-6'>
-                <Link to="/dash">
-                    <img src={home} alt="home icon" className='cursor-pointer'/>
-                </Link>
-            </div>
-            <input
-                type="text"
-                value={recipientId}
-                onChange={(e) => setRecipientId(e.target.value)}
-                placeholder="Enter recipient UUID"
-                className="border-none py-2 bg-[#322727] text-center text-[#515151] placeholder-[#bfbfbf] rounded-md w-full max-w-md font-roboto"
-            />
-            <div className="w-full max-w-md border-none p-4 h-64 overflow-y-scroll bg-[#241919] rounded-md font-roboto no-scrollbar">
-                {messages.length === 0 ? (
-                    <p className="text-white font-roboto">No messages yet.</p>
-                ) : (
-                    messages.map((msg, index) => (
-                        <div
-                            key={index}
-                            className={`mb-2 flex ${msg.sender === senderName ? 'justify-end' : 'justify-start items-center'} font-roboto`}
-                        >
-                            {msg.sender !== senderName && (
-                                <>
-                                    <img src={UserIcon} alt="User Icon" className="w-8 h-8 mr-4" />
-                                    <div className="text-white font-roboto">
-                                        <span className="block text-xs text-gray-300 font-roboto">
-                                            {msg.sender} - {new Date(msg.timestamp).toLocaleTimeString()}
-                                        </span>
-                                        <p className="font-roboto">{msg.message}</p>
-                                    </div>
-                                </>
-                            )}
-                            {msg.sender === senderName && (
+        <div className="flex h-screen bg-[#000] font-roboto">
+            {/* Main chat area */}
+            <div className="flex-1 flex flex-col items-center justify-center p-4">
+                <div className='absolute z-10 w-8 h-auto top-6 left-6'>
+                    <Link to="/dash">
+                        <img src={home} alt="home icon" className='cursor-pointer'/>
+                    </Link>
+                </div>
+                <div className="w-full max-w-md flex flex-col">
+                    <input
+                        type="text"
+                        value={recipientId}
+                        placeholder="Recipient ID"
+                        disabled
+                        className="border-none py-2 bg-[#322727] text-center text-[#515151] placeholder-[#bfbfbf] rounded-md w-full max-w-md mb-4"
+                    />
+                    <div className="w-full max-w-md border-none p-4 h-64 overflow-y-scroll bg-[#241919] rounded-md no-scrollbar">
+                        {messages.length === 0 ? (
+                            <p className="text-white">No messages yet.</p>
+                        ) : (
+                            messages.map((msg, index) => (
                                 <div
-                                    className="p-2 bg-[#150E0E] text-white rounded-[20px] px-4 font-roboto"
-                                    style={{ maxWidth: '60%' }}
+                                    key={index}
+                                    className={`mb-2 flex ${msg.sender === senderName ? 'justify-end' : 'justify-start items-center'}`}
                                 >
-                                    <span className="block text-xs text-gray-500 font-roboto">
-                                        {msg.sender} - {new Date(msg.timestamp).toLocaleTimeString()}
-                                    </span>
-                                    <p className="font-roboto">{msg.message}</p>
+                                    {msg.sender !== senderName && (
+                                        <>
+                                            <img src={UserIcon} alt="User Icon" className="w-8 h-8 mr-4" />
+                                            <div className="text-white">
+                                                <span className="block text-xs text-gray-300">
+                                                    {msg.sender} - {new Date(msg.timestamp).toLocaleTimeString()}
+                                                </span>
+                                                <p>{msg.message}</p>
+                                            </div>
+                                        </>
+                                    )}
+                                    {msg.sender === senderName && (
+                                        <div
+                                            className="p-2 bg-[#150E0E] text-white rounded-[20px] px-4"
+                                            style={{ maxWidth: '60%' }}
+                                        >
+                                            <span className="block text-xs text-gray-500">
+                                                {msg.sender} - {new Date(msg.timestamp).toLocaleTimeString()}
+                                            </span>
+                                            <p>{msg.message}</p>
+                                        </div>
+                                    )}
                                 </div>
-                            )}
-                        </div>
-                    ))
-                )}
+                            ))
+                        )}
+                    </div>
+                </div>
+
+                <div className="relative w-full max-w-md mb-4">
+                    <input
+                        type="text"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder="Send an inspirational message..."
+                        onKeyDown={handleKeyPress}
+                        className="w-full italic p-2 bg-[#241919] text-white placeholder-gray-500 rounded-md border border-[#FF833A] focus:outline-none"
+                    />
+                    <img
+                        src={SendIcon}
+                        alt="Send"
+                        onClick={sendMessage}
+                        className={`absolute right-3 hover:scale-75 top-1/2 transform -translate-y-1/2 h-5 w-auto cursor-pointer transition-all duration-300 ${
+                            isShrinking ? 'scale-75' : 'scale-100'
+                        }`}
+                    />
+                </div>
+
+                <p className="text-[#8c8c8c]">User ID: {userId}</p>
+
+                <div className="absolute bottom-20 left-20">
+                    <Link to="/dash">
+                        <img src={Logo} alt="Logo" />
+                    </Link>
+                </div>
             </div>
 
-            <div className="relative w-full max-w-md mb-4 font-roboto">
-                <input
-                    type="text"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    placeholder="Send an inspirational message..."
-                    onKeyDown={handleKeyPress}
-                    className="w-full italic p-2 bg-[#241919] text-white placeholder-gray-500 rounded-md border border-[#FF833A] focus:outline-none font-roboto"
-                />
-                <img
-                    src={SendIcon}
-                    alt="Send"
-                    onClick={sendMessage}
-                    className={`absolute right-3 hover:scale-75 top-1/2 transform -translate-y-1/2 h-5 w-auto cursor-pointer transition-all duration-300 ${
-                        isShrinking ? 'scale-75' : 'scale-100'
-                    }`}
-                />
-            </div>
+            {/* Right-side contact list */}
+            <div className="w-1/3 bg-[#1a1a1a] p-4 text-white">
+                <h2 className="text-xl font-semibold mb-4">Contacts</h2>
+                <ul className="space-y-4">
+                    {contacts.map((contact) => (
+                        <li key={contact.uuid} onClick={() => selectContact(contact.uuid)} className="cursor-pointer hover:text-[#FF833A] transition">
+                            <div className="flex items-center space-x-2">
+                                <img src={UserIcon} alt="User Icon" className="w-6 h-6" />
+                                <span>{contact.name}</span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
 
-            <p className="text-[#8c8c8c] font-roboto mt-2">User ID: {userId}</p>
-
-            <div className="absolute bottom-20 left-20 font-roboto">
-                <Link to="/dash">
-                    <img src={Logo} alt="Logo" className="bottom-10 left-10" />
-                </Link>
+                {/* Add contact input and plus icon */}
+                <div className="mt-8 flex items-center">
+                    <input
+                        type="text"
+                        value={newContactUUID}
+                        onChange={(e) => setNewContactUUID(e.target.value)}
+                        placeholder="Enter contact UUID"
+                        className="flex-1 p-2 bg-[#241919] text-white rounded-md placeholder-gray-500 border border-[#FF833A] focus:outline-none"
+                    />
+                    <img
+                        src={PlusIcon}
+                        alt="Add"
+                        onClick={addContact}
+                        className="w-8 h-8 ml-4 cursor-pointer hover:scale-110 transition-all duration-300"
+                    />
+                </div>
             </div>
         </div>
     );
